50b536e1389f8c308d2dcc343d9a3eacb1d921d2
Miguel (SACYL) <ariesmoo@gmail.com>
2018-01-24 14:45:07 +0100

    Avanzando en el 112

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Aux from "../../hoc/Auxiliarity";
 
+import Classes from "./Layout.css"
+
 // OJO !!! son parentesis (no llaves, pq si no deberia llevar un return)
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={Classes.Content}>
             {props.children}
         </main>
     </Aux>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import Aux from '../../hoc/Auxiliarity'
+
+export default class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger Controls</div>
+            </Aux>
+        )
+    }
+}
