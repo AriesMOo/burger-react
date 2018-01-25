f17dff6f99f0e5a044646eb510e091d3909bb3fe
Miguel (SACYL) <ariesmoo@gmail.com>
2018-01-25 14:48:54 +0100

    Dinamismos en ingredientes (class 114)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const burger = (props) => {
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import Classes from './Burger.css'
+
+const burger = (props) => { 
+    let transformedIngredients = 
+      Object.keys(props.ingredients)
+            .map(igKey => {
+                return [...Array(props.ingredients[igKey] )].map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />
+                });
+            })
+            .reduce((arr, el) => {
+                return arr.concat(el);
+            }, []);
+    
+    // console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Por favor, añade ingredientes!</p>
+    }
+
     return (
-        <div>
-            
+        <div className={Classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
         </div>
     )
 }
