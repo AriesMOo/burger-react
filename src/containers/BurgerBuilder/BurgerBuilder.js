875b3c8d4cd0bb1b38a82a5595984c3724b4e6c2
Miguel Priede <ariesmoo@gmail.com>
2018-01-25 21:18:46 +0100

    Bueno.. pues a la 117 con botones de control

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Burger from "../../components/Burger/Burger";
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Aux from '../../hoc/Auxiliarity'
 
 export default class BurgerBuilder extends Component {
@@ -22,7 +23,7 @@ export default class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger Controls</div>
+                <BuildControls/>
             </Aux>
         )
     }
